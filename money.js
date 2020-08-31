8799109ca6227ab18a44f0f964517ff225a5ccb9
Natacha <natacha.har@onja.org>
2020-08-31 11:05:11 +0300

    Convert currency

--- money.js
+++ money.js
@@ -1,3 +1,14 @@
+const fromSelect = document.querySelector('[name="from_currency"]');
+const toSelect = document.querySelector('[name="to_currency"]');
+const fromInput = document.querySelector('[name="from_amount"]');
+const toEL = document.querySelector('.to_amount');
+const form = document.querySelector('.app form');
+
+const endpoint = 'https://api.exchangeratesapi.io/latest';
+// cach object, where we are going to store the rates that we already
+const ratesByBase = {};
+
+
 const currencies = {
 	USD: 'United States Dollar',
 	AUD: 'Australian Dollar',
@@ -32,3 +43,59 @@ const currencies = {
 	ZAR: 'South African Rand',
 	EUR: 'Euro',
 };
+
+function generateOptions(options) {
+	return Object.entries(options).map(([currencyCode, currencyName]) => {
+		// we destructor the array directly in the parameter definition
+		return `
+		<option value="${currencyCode}">
+			${currencyCode} - ${currencyName}
+		</option>
+		`;
+	}).join('');
+}
+
+// default base parameter  set to USD
+async function fetchRates(base = 'USD') {
+	const res = await fetch(`${endpoint}?base=${base}`);
+	const rates = await res.json();
+	return rates;
+}
+
+async function convert(amount, from, to) {
+	// first check if we even have the rates to convert from that currency
+	if (!ratesByBase[from]) {
+		console.log(`Oh no we don't have ${from} to convert to ${to}, Let's get it`);
+		const rates = await fetchRates(from);
+		console.log(rates);
+		// store them for next time ! if CAD, store then to 'ratesByBase .CAD
+		ratesByBase[from] = rates;
+	}
+	const rate = ratesByBase[from].rates[to];
+	const convertedAmount = rate * amount;
+	console.log(`${amount} ${from} is ${convertedAmount} in ${to}`);
+	return convertedAmount;
+}
+
+
+const optionsHTML = generateOptions(currencies);
+fromSelect.innerHTML = optionsHTML;
+toSelect.innerHTML = optionsHTML;
+
+function formatCurrency(amount, currency) {
+	return Intl.NumberFormat('fr-FR', {
+		style: "currency",
+		currency,
+	}).format(amount);
+}
+
+async function handleInput(e) {
+	const rawAmount = await convert(
+		fromInput.value, 
+		fromSelect.value, 
+		toSelect.value);
+	console.log(rawAmount);
+	toEL.textContent = formatCurrency(rawAmount, toSelect.value);
+}
+
+form.addEventListener('input', handleInput);
